371ea0e24b354e871d890015ea6a42f9505a4e85
Milenkkkaa <msma91588@gmail.com>
2024-11-17 18:01:37 +0200

    Add files via upload

---
+++ lab7/lab7/House.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public class House : IComparable
+    {
+        public double width { get; set; }
+        public double length { get; set; }
+        public double height { get; set; }
+        public int room { get; set; }
+        public int floor { get; set; }
+        public double value { get; set; }
+        public double price { get; set; }
+        public bool hasForniture { get; set; }
+        public double getCost()
+        {
+
+            return (width * length) * price;
+        }
+        public double heating()
+        {
+            return Heating;
+        }
+        public double GetCost
+        {
+            get
+            {
+                return getCost();
+            }
+            set
+            {
+            }
+        }
+        public double Heating
+        {
+            get
+            {
+                return heating();
+            }
+            set
+            {
+            }
+        }
+        public House()
+        {
+
+        }
+        public House(double Width, double Length,
+            double Height, int Room, int Floor,
+            double Value, double Price, bool HasForniture,
+            double GetCost, double Heating)
+        {
+            width = Width;
+            length = Length;
+            height = Height;
+            room = Room;
+            floor = Floor;
+            value = Value;
+            price = Price;
+            hasForniture = HasForniture;
+        }
+        public string Info()
+        {
+            return price + ", " + width + ", " + length + ", " + height;
+        }
+       public int CompareTo(object obj)
+        {
+            if (obj == null) return 1; 
+            House house = obj as House;
+            if (house == null) throw new ArgumentException("Object is not a House");
+            return this.price.CompareTo(house.price); 
+        }
+    }
+}
---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,89 @@
+﻿using Lab7;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab7
+{
+    internal class Program
+    {
+        static List<House> houses;
+        static void PrintHouses()
+        {
+            foreach (House house in houses)
+            {
+                Console.WriteLine(house.Info().Replace('i', 'i'));
+            }
+        }
+        static void Main(string[] args)
+        {
+            houses = new List<House>();
+            FileStream fs = new FileStream("binary.houses", FileMode.Open);
+            BinaryReader reader = new BinaryReader(fs);
+            try
+            {
+                House tempHouse;
+                Console.WriteLine("Читаєсо дані з файлу...\n");
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    tempHouse = new House();
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        switch (i)
+                        {
+                            case 1:
+                                tempHouse.width = reader.ReadDouble(); break;
+                            case 2:
+                                tempHouse.length = reader.ReadDouble(); break;
+                            case 3:
+                                tempHouse.height = reader.ReadDouble(); break;
+                            case 4:
+                                tempHouse.room = reader.ReadInt32(); break;
+                            case 5:
+                                tempHouse.floor = reader.ReadInt32(); break;
+                            case 6:
+                                tempHouse.value = reader.ReadDouble(); break;
+                            case 7:
+                                tempHouse.price = reader.ReadDouble(); break;
+                            case 8:
+                                tempHouse.hasForniture = reader.ReadBoolean(); break;
+                            case 9:
+                                tempHouse.GetCost = reader.ReadDouble(); break;
+                            case 10:
+                                tempHouse.Heating = reader.ReadDouble(); break;
+                                }
+                        }
+                        houses.Add(tempHouse);
+                    }
+                }
+                catch (Exception ex)
+            {
+                Console.WriteLine("Сталась помилка: \n{0}", ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+            }
+            Console.WriteLine("Несортований перелік будинків: {0}", houses.Count);
+            PrintHouses();
+            houses.Sort();
+            Console.WriteLine("Сортований перлік будинків: {0}", houses.Count);
+            PrintHouses();
+            Console.WriteLine("Додаємо новий запис:");
+            House house = new House(15, 25, 4, 5, 1, 16, 45000, false, 16875000, 6000);
+            houses.Add(house);
+            houses.Sort();
+            Console.WriteLine("Перелік будинків: {0}", houses.Count());
+            PrintHouses();
+            Console.WriteLine("Видаляємо останнє значення");
+            houses.RemoveAt(houses.Count - 1);
+            Console.WriteLine("Перелік будинків: {0}", houses.Count);
+            PrintHouses();
+            Console.ReadKey();
+        }
+    }
+}
